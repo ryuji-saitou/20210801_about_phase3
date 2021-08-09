7a505279718f9fa83b23d61b364d2311f752b4a3
ryuji-saitou <rs0623idmanagement@gmail.com>
2021-08-09 10:53:35 +0000

    [add] ルーティング記述(ラフ)完了

--- app/controllers/public/reports_controller.rb
+++ app/controllers/public/reports_controller.rb
@@ -1,9 +1,15 @@
 class Public::ReportsController < ApplicationController
 
-  def create
+  def post_report_create
   end
 
-  def destroy
+  def post_report_destroy
+  end
+
+  def comment_report_create
+  end
+
+  def comment_report_destroy
   end
 
 end
