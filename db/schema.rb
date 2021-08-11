b76c1e071e052fd7c68384d5c4634aa58ee218cd
ryuji-saitou <rs0623idmanagement@gmail.com>
2021-08-11 08:12:13 +0000

    [fix] user_name_idにunique制約追加 & db/migrate/NO FILE削除

--- db/schema.rb
+++ db/schema.rb
@@ -117,7 +117,6 @@ ActiveRecord::Schema.define(version: 2021_08_09_121739) do
     t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role", default: 1, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
