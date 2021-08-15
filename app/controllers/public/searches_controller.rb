1603fc8784231c2ea4fdc4104909e25f4fadc0aa
ryuji-saitou <rs0623idmanagement@gmail.com>
2021-08-15 02:35:25 +0000

    [add] search/post検索(ラフ)実装

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,9 +1,25 @@
 class Public::SearchesController < ApplicationController
   def search
+    @search_params = search_params
   end
 
   def search_result
-    @user = User.find(1)
+    @search_params = search_params
+    @posts = Post.post_search(@search_params)
     render :search
+  end
+
+  private
+  def search_params
+    params.fetch(:search, {}).permit(
+      :search_target,
+      :action,
+      :time_required_from,
+      :time_required_to,
+      :budget_from,
+      :budget_to,
+      :created_at_from,
+      :created_at_to
+    )
   end
 end
