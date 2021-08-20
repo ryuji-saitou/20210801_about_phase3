22da057ff64dee2ee3c444c8326979efbf5ceb86
ryuji-saitou <rs0623idmanagement@gmail.com>
2021-08-20 05:40:27 +0000

    [add] 「コメント」非同期通信実装

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -4,15 +4,19 @@ class Public::CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @comment.post_id = @post.id
     @comment.user_id = current_user.id
-    @comment.save
-    @post.create_notification_comment!(current_user, @comment.id)
-    redirect_to post_path(@post.id)
+    if @comment.save
+      @post.create_notification_comment!(current_user, @comment.id)
+    else
+      redirect_to post_path(@post)
+    end
   end
 
   def destroy
     @post = Post.find(params[:post_id])
-    Comment.find_by(post_id: params[:post_id], id: params[:id]).destroy
-    redirect_to post_path(@post.id)
+    if Comment.find_by(post_id: params[:post_id], id: params[:id]).destroy
+    else
+      redirect_to post_path(@post)
+    end
   end
 
   private
