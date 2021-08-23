008511875a38c07ef2a5078876366f9a5687b1a0
ryuji-saitou <rs0623idmanagement@gmail.com>
2021-08-23 12:34:18 +0000

    [fix] config/database.yml修正

--- app/controllers/public/reports_controller.rb
+++ app/controllers/public/reports_controller.rb
@@ -30,4 +30,6 @@ class Public::ReportsController < ApplicationController
     @report = Report.find_by(post_id: @post.id, comment_id: @comment.id, user_id: current_user)
     @report.destroy
   end
+
+  # test
 end
