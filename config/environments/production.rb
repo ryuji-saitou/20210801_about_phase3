bfefc681742a24aadad5ce5dde617c20a8e184c8
ryuji-saitou <rs0623idmanagement@gmail.com>
2021-08-26 15:51:27 +0000

    [fix] デプロイ後、実際のページでメール問い合わせが送信できない問題修正完了

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # ======メール問い合わせ記述箇所======
+  # ======メール問い合わせ記述箇所(development.rbの記述を転載)======
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
@@ -104,6 +104,6 @@ Rails.application.configure do
       password: ENV['SMTP_PASSWORD'],
       enable_starttls_auto: true
   }
-  # ========================================
+  # ================================================================
 
 end
